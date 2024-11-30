e7111bd27d5a3d9cd8064de17fd8289698ad1a41
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-11-30 15:08:47 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,11 @@
+def maximum_of_three(num):
+    a = max(num)
+    return a
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+lst = [num1,num2,num3]
+print(maximum_of_three(lst))
+
+
